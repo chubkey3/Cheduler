f709eb190a1fdd27b55f8f7218707337eeecbc16
Jason Louie <jason.jw.louie@gmail.com>
2022-06-12 18:24:56 -0700

    idk bro

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const app = express();
+
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3001;
 
 const tasksRoute = require('./routes/tasks')
